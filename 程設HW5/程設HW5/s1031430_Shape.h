e496d2f32fed09eacfdca43ac839dd968f997234
s1031430 <w5151381guy@gmail.com>
2015-06-10 23:23:47 +0800

    modify HW5

--- 程設HW5/程設HW5/s1031430_Shape.h
+++ 程設HW5/程設HW5/s1031430_Shape.h
@@ -1,5 +1,8 @@
 #pragma once
-#include "hw5_main.h"
+#include "s1031430_hw5_main.h"
+#include<vector> 
+
+//isConvex == true ¬°¥Y ¡A else ¬°¥W
 
 namespace MIME{
 	class Vertex{
@@ -7,23 +10,24 @@ namespace MIME{
 		Vertex() : x(0.0f), y(0.0f){}
 		Vertex(const float inX, const float inY) : x(inX), y(inY){}
 		void print(){ std::cout << "(" << x << "," << y << ")"; }
-
+		
 		float x, y;
 	};
 
-	class Shape{
+	class Shape :public Vertex
+	{
 	public:
 		Shape() : CurrentType(ShapeType::Shape){}
 
 		//add a Vertex
 		size_t addVertex(const Vertex inV){ vertices.push_back(inV); return vertices.size(); }
 		size_t addVertex(const float inX, const float inY){ vertices.push_back(Vertex(inX, inY)); return vertices.size(); }
-
-		//??? void print();
-		//??? ShapeType getType();
-		//??? float getArea();
-		//??? bool isValid();
-		//??? bool isConvex();
+		void print();
+		virtual float getArea() = 0;
+		virtual bool isValid() = 0;
+		bool isConvex();
+		Vertex A, B, C, D, E;
+		ShapeType getType();
 	protected:
 		ShapeType CurrentType;
 		std::vector<Vertex> vertices;
@@ -31,7 +35,72 @@ namespace MIME{
 
 	class Triangle : public Shape{
 	public:
-		/* ... */
+		Triangle();
+		float getArea();
+		bool isValid();
 	};
 
+	class Quadrilateral : public Shape
+	{
+	public:
+		Quadrilateral();
+		float getArea();
+		bool isValid();
+	};
+
+	class Pentagon : public Shape
+	{
+	public:
+		Pentagon();
+		float getArea();
+		bool isValid();
+	};
+
+	class Parallelogram : public Shape
+	{
+	public:
+		Parallelogram();
+		float getArea();
+		bool isValid();
+	};
+
+	class Square : public Shape
+	{
+	public:
+		Square();
+		float getArea();
+		bool isValid();
+	};
+
+	class Trapezium : public Shape
+	{
+	public:
+		Trapezium();
+		float getArea();
+		bool isValid();
+	};
+
+	class Isosceles_triangle : public Shape
+	{
+	public:
+		Isosceles_triangle();
+		float getArea();
+		bool isValid();
+	};
+
+	class Regular_triangle : public Shape
+	{
+	public:
+		Regular_triangle();
+		float getArea();
+		bool isValid();
+	};
+
+	class Right_triangle : public Shape
+	{
+	public:
+		Right_triangle();
+		float getArea();
+		bool isValid();
+	};
 }
--- 程設HW5/程設HW5/s1031430_hw5_main.cpp
+++ 程設HW5/程設HW5/s1031430_hw5_main.cpp
@@ -1,5 +1,6 @@
-#include "hw5_main.h"
-#include "Shape.h"
+#include<iostream>
+#include "s1031430_hw5_main.h"
+#include "s1031430_Shape.h"
 
 std::vector<MIME::Shape*> data;
 
@@ -24,17 +25,110 @@ int loadShapes(std::string file )
 			std::cout << "[LOADER] invalde abstract type: Shape" << std::endl;
 			return NUMofSHAPE;
 
-		//case Triangle:
-		//	std::cout << "[LOADER] add: Triangle" << std::endl;
-		//	data.push_back(new MIME::Triangle());
-		//	/* ... */
-		//	break;
+		case Triangle:
+		std::cout << "[LOADER] add: Triangle" << std::endl;
+		data.push_back(new MIME::Triangle());
+		for (int i = 0; i < 3; i++)
+		{
+			infile >> inX >> inY;
+			(data.back())->addVertex(inX, inY);
+		}
+		NUMofSHAPE++;
+		break;
+
+		case Quadrilateral:
+			std::cout << "[LOADER] add: Quadrilateral" << std::endl;
+			data.push_back(new MIME::Quadrilateral());
+			for (int i = 0; i < 4; i++)
+			{
+				infile >> inX >> inY;
+				(data.back())->addVertex(inX, inY);
+			}
+			NUMofSHAPE++;
+			break;
+
+		case Pentagon:
+			std::cout << "[LOADER] add: Pentagon" << std::endl;
+			data.push_back(new MIME::Pentagon());
+			for (int i = 0; i < 5; i++)
+			{
+				infile >> inX >> inY;
+				(data.back())->addVertex(inX, inY);
+			}
+			NUMofSHAPE++;
+			break;
+
+		case Parallelogram:
+			std::cout << "[LOADER] add: Parallelogram" << std::endl;
+			data.push_back(new MIME::Parallelogram());
+			for (int i = 0; i < 4; i++)
+			{
+				infile >> inX >> inY;
+				(data.back())->addVertex(inX, inY);
+			}
+			NUMofSHAPE++;
+			break;
+
+		case Square:
+			std::cout << "[LOADER] add: Square" << std::endl;
+			data.push_back(new MIME::Square());
+			for (int i = 0; i < 4; i++)
+			{
+				infile >> inX >> inY;
+				(data.back())->addVertex(inX, inY);
+			}
+			NUMofSHAPE++;
+			break;
+
+		case Trapezium:
+			std::cout << "[LOADER] add: Trapezium" << std::endl;
+			data.push_back(new MIME::Trapezium());
+			for (int i = 0; i < 4; i++)
+			{
+				infile >> inX >> inY;
+				(data.back())->addVertex(inX, inY);
+			}
+			NUMofSHAPE++;
+			break;
+
+		case Isosceles_triangle:
+			std::cout << "[LOADER] add: Isosceles_triangle" << std::endl;
+			data.push_back(new MIME::Isosceles_triangle());
+			for (int i = 0; i < 3; i++)
+			{
+				infile >> inX >> inY;
+				(data.back())->addVertex(inX, inY);
+			}
+			NUMofSHAPE++;
+			break;
+
+		case Regular_triangle:
+			std::cout << "[LOADER] add: Regular_triangle" << std::endl;
+			data.push_back(new MIME::Regular_triangle());
+			for (int i = 0; i < 3; i++)
+			{
+				infile >> inX >> inY;
+				(data.back())->addVertex(inX, inY);
+			}
+			NUMofSHAPE++;
+			break;
+
+		case Right_triangle:
+			std::cout << "[LOADER] add: Right_triangle" << std::endl;
+			data.push_back(new MIME::Right_triangle());
+			for (int i = 0; i < 3; i++)
+			{
+				infile >> inX >> inY;
+				(data.back())->addVertex(inX, inY);
+			}
+			NUMofSHAPE++;
+			break;
 
 		default:
 			std::cout << "[LOADER] under construction: " << ShapeName[n] << std::endl;
 			infile.getline(empty,255);	//eat line
 			NUMofSHAPE++;
-			//return NUMofSHAPE;
+			return NUMofSHAPE;
 		}
 	}
 
@@ -52,19 +146,10 @@ int main()
 	//print out each sahpe
 	for each (MIME::Shape* var in data)
 	{
-	//	var->print();
-	//	std::cout << "AREA: " << var->getArea() << std::endl;
-	//	std::cout << "Valid?: " << (var->isValid() ? "YES" : "NO") << std::endl;
-	//	std::cout << "Convex?: " << (var->isConvex() ? "YES" : "NO") << std::endl << std::endl;
-	
-	//**SAMPLE**
-	//TYPE: Isosceles_triangle
-	//(-2, 0)
-	//(0, 6)
-	//(2, 0)
-	//AREA: 12
-	//Valid?: YES
-	//Convex?: YES
+		var->print();
+		std::cout << "AREA: " << var->getArea() << std::endl;
+		std::cout << "Valid?: " << (var->isValid() ? "YES" : "NO") << std::endl;
+		std::cout << "Convex?: " << (var->isConvex() ? "YES" : "NO") << std::endl << std::endl;
 	}
 
 	for each (MIME::Shape* var in data)
